73e9d4eff61715c777ca76303a4deeed9b0bb9e7
Tony Edwards <tony.edwards@gmail.com>
2020-05-13 12:35:31 +0100

    Serves different sized background images to different screen sizes

--- gulpfile.js
+++ gulpfile.js
@@ -25,3 +25,50 @@ gulp.task('img', function() {
     .pipe(imagemin())
     .pipe(gulp.dest(`images/speakers/optimised`));
 });
+
+
+// header images
+gulp.task('header-img', function() {
+    return gulp.src(`images/backgrounds/*.jpg`)
+      .pipe($.responsive({
+        '*': [{
+          width: 540,
+          rename: {
+            suffix: '_xs' ,
+            extname: '.jpeg',
+          },
+        },{
+          width: 768,
+          rename: {
+            suffix: '_sm',
+            extname: '.jpeg',
+          }
+        }, {
+          width: 992,
+          rename: {
+            suffix: '_md',
+            extname: '.jpeg',
+          }
+        },{
+          width: 1299,
+          rename: {
+            suffix: '_lg',
+            extname: '.jpeg',
+          }
+        },{
+          width: 1920,
+          rename: {
+            suffix: '_xl',
+            extname: '.jpeg',
+          }
+        }],
+      },
+      {
+        quality: 85,
+        progressive: true,
+        withMetadata: false,
+        errorOnEnlargement: false,
+      }))
+      .pipe(imagemin())
+      .pipe(gulp.dest(`images/backgrounds/`));
+  });
